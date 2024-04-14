6af8e13571f846f96c45ac3b1d8377ad0acc808e
m0x0m0x <mx0mx@tuta.io>
2024-04-14 12:15:22 +0530

    add sound and confetti

---
+++ W/soun.js
@@ -0,0 +1,29 @@
+/* 
+This is for adding sound effects which are taken from here 
+main Sound - https://cdn.freesound.org/previews/721/721989_12068263-lq.mp3 
+Source Web - https://freesound.org/search/?f=grouping_pack:%2239000_Female%20Sexual%20Sound%20Effects%20For%20NSFW%20Content%20Creators%22&s=Date+added+(newest+first)&g=1
+*/
+
+//////// Template
+
+//////////////////////////////
+// === Login Button Sond ===
+
+const Logbtn = document.querySelector(".login__btn");
+const clickSound = document.getElementById("clickSound");
+Logbtn.addEventListener("click", () => {
+  clickSound.currentTime = 0; // Reset the audio to the beginning
+  clickSound.play(); // Play the click sound
+});
+
+//////////////////////////////
+// === OpenAccountSond ===
+
+const opnbtn = document.querySelector(
+  ".nav__link.nav__link--btn.btn--show-modal"
+);
+const openSound = document.getElementById("openSound");
+Logbtn.addEventListener("click", () => {
+  openSound.currentTime = 0; // Reset the audio to the beginning
+  openSound.play(); // Play the click sound
+});
