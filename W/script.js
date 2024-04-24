ec511e4a25c2e0afc42b204d06fd9a74288ee542
m0x0m0x <mx0mx@tuta.io>
2024-04-24 16:36:57 +0530

    add

--- W/script.js
+++ W/script.js
@@ -303,6 +303,16 @@ const createDots = function () {
 
 createDots();
 
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}]`)
+    .classList.add(".dots__dot--active");
+};
+
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
@@ -318,6 +328,7 @@ const nextSlide = function () {
     curSlide++;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 const prevSlide = function () {
@@ -327,6 +338,7 @@ const prevSlide = function () {
     curSlide--;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 btnRight.addEventListener("click", nextSlide);
@@ -345,6 +357,7 @@ dotContainer.addEventListener("click", function (e) {
   if (e.target.classList.contains("dots__dot")) {
     const { slide } = e.target.dataset;
     goToSlide(slide);
+    activateDot(slide);
   }
 });
 
